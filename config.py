82ebad6427265cd3894f85ecc9f499016ec63209
Alexander Danson <alex.danson2@gmail.com>
2020-04-10 18:38:54 +0200

    fix bulk upload bug

--- config.py
+++ config.py
@@ -6,7 +6,6 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
                               'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER = os.path.join(basedir, 'uploads/')
     MAX_CONTENT_PATH = 100000
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
